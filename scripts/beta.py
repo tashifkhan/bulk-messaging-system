4f9d5ceae535353d8a8b23a32bf79ab0d7cd6f19
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2023-12-29 03:07:59 +0530

    reverting major flailpoints

--- scripts/beta.py
+++ scripts/beta.py
@@ -1,37 +1,62 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for, flash, session 
-from werkzeug.utils import secure_filename
-app = Flask(__name__)
+'''
+Design a Whatsapp Messaging System.
+    A service in which user can send a message to all the whatsapp number present in csv file
+    A service in which user can schedule the message to be sent at particular time to all the whatsapp number specified
+    A service which allows you to filter specific phone numbers from the csv/excel file to send the message
+'''
+# Include all the necessary libraries
+import csv
+import pywhatkit
+import schedule
+import time
 
-# ALLOWED_EXTENSIONS = {'csv', 'xlsx', 'xls'}
-# def allowed_file(filename):
-#     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+import tkinter as tk
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the postgit  request has the file part
-        if 'file' not in request.files:
-            return render_template('index.html', error='No file part')
+def send_messages_to_all(file_path, message):
+    # Reads numbers from a CSV file and sends the message to each.
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        # Check for header like konse column mai numbers stored hai 👆
+        for row in reader:
+            number = row[header.index('Phone No.')] 
+            pywhatkit.sendwhatmsg_instantly(number, message)
 
-        file = request.files['file']
+def schedule_message(file_path, message, time_hour, time_minute):
+    # Schedules a message to be sent at the specified time.
+    schedule.every().day.at(f"{time_hour}:{time_minute}").do(send_messages_to_all, file_path, message) # 24 hour format
 
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            return render_template('index.html', error='No selected file')
+def filter_and_send(file_path, message, filter_criteria):
+    # Filters numbers based on criteria and sends the message.
+    filtered_numbers = []
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        header = next(reader)  # Skip header row along with stored in a list header
+        for row in reader:
+            if filter_criteria(row):  # Implement your filtering logic here
+                filtered_numbers.append(row[0])
 
-        # Save the file to the desired location
-        filename = secure_filename(file.filename)
-        file.save(os.path.join('uploads/', filename))
+    for number in filtered_numbers:
+        pywhatkit.sendwhatmsg_instantly(number, message)
 
-        return render_template('index.html', success='File successfully uploaded')
 
-    return render_template('index.html')
+# Example usage:
+file_path = "contacts.csv"
+message = "Hello from the WhatsApp Messaging System!"
+time_hour = 10
+time_minute = 30
 
-# @app.route('/')
-# def hello_world():
-#     return render_template('index.html')
+'''
+before running this code, open whatsapp web in your browser and you have to be logged in 
+otherwise it will not work
+'''
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# testing
+send_messages_to_all(file_path, message)
+# schedule_message(file_path, message, time_hour, time_minute)
+# filter_and_send(file_path, message, lambda row: row[1] == "Country")  # Example filter
+
+# Keep the program running for scheduled tasks
+while True:
+    schedule.run_pending()
+    time.sleep(1)
