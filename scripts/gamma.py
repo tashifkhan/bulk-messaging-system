90998f264d0a271854d718cff8b2c5ec4f597e70
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2023-12-29 03:07:59 +0530

    reverting major flailpoints

---
+++ scripts/gamma.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium. webdriver. chrome.service import Service
+from webdriver_manager. chrome import ChromeDriverManager
+from selenium. webdriver. common. action_chains import ActionChains
+from selenium. webdriver. common.keys import Keys
+from urllib.parse import quote
